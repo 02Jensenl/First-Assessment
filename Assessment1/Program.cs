957ca0c5769e19cecd6e72a9326c98781c0a8db2
Albert Ngoudjou <35413177+angoudjou@users.noreply.github.com>
2020-10-20 09:12:38 -0400

    Initial commit

---
+++ Assessment1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assessment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
